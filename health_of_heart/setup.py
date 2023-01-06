65d10aab8ad4d8fc8cc08900c8c9f7f72b9ae2f7
Daniel Schulz <danielschulz2005@hotmail.com>
2023-01-06 16:10:14 +0100

    add HoH sub-repo

---
+++ health_of_heart/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyze risk factors of strokes in order to prevent and predict fatal instances.',
+    author='Daniel Schulz',
+    license='MIT',
+)
